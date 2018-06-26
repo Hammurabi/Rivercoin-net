7c44755f0f835ee7fd7232bbb5d72e7256f32272
Jacob Møller <jmj1999@gmail.com>
2018-06-26 20:13:46 +0200

    Updated font and added exchange page

---
+++ front-end/exchange/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Rubik" rel="stylesheet">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../../common-content/master.css">
+	<link rel="stylesheet" type="text/css" href="custom.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<header class="wrapper noselect">
+	<nav>
+		<img src="../../common-content/image-content/logofull.png">
+		<div class="hamburgermenu" onclick="hamburgerMenu()">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<ul id="menu">
+			<li>Why</li>
+			<li>How it works</li>
+			<li>Our Visions</li>
+			<li class="calltoaction">Get it!</li>
+		</ul>
+	</nav>
+</header>
+<section class="wrapper topsection secondary-dark exchange">
+<h1>Exchange</h1>
+<?php
+echo "Currency 1:";
+echo "Currency 2:";
+echo "Current Fee:";
+echo "Next Fee:";
+
+
+?>
+hej<br>fdihej<br>fdihej<br>fdihej<br>fdihej<br>fdihej<br>fdis
+</section>
+</body>
+</html>
