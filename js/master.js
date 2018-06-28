68d385f6d05467067f6aed6500dac1c74ea331a9
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-06-28 07:21:54 +0200

    some simple updates

--- js/master.js
+++ js/master.js
@@ -1,17 +1,39 @@
-function hamburgerMenu() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+// function hamburgerMenu() {
+//     var x = document.getElementById("menu");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+// window.onresize = function(event) {
+//     var screensize = $(window).width();
+//     if (screensize > 763) {
+//     	document.getElementById("menu").style.display = "block";
+//     }
+//     else{
+//     	document.getElementById("menu").style.display = "none";
+//     }
+// };
+
+function burgerDisplay()
+{
+    if(window.innerWidth <= 780)
+        document.getElementById("collapseable-menu").style.display = "inline-block";
 }
-window.onresize = function(event) {
-    var screensize = $(window).width();
-    if (screensize > 763) {
-    	document.getElementById("menu").style.display = "block";
+
+var lastWidth = window.innerWidth;
+
+window.addEventListener('resize', ()=>{
+    if(window.innerWidth > 780)
+    {
+        document.getElementById("burger-checkbox").checked = false;
+        document.getElementById("collapseable-menu").style.display = "inline-block";
     }
-    else{
-    	document.getElementById("menu").style.display = "none";
+    else if(window.innerWidth <= 780 && lastWidth > 780)
+    {
+        document.getElementById("collapseable-menu").style.display = "none";
     }
-};
+
+    lastWidth = window.innerWidth;
+});
